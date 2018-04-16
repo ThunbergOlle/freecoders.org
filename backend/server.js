658fafffe7df9aee859e87838ea249f215ae5eb7
Olle Thunberg <olle.thunberg03@gmail.com>
2018-04-16 20:13:47 +0200

    Login / Signup System & Sessions
    Signup and login system is now fully working. Sessions are also fully functional (kind of). I'm going to go deeper into sessions ect later.
    
    I took me a while but I finally figured out how sessions are going to play out.

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,19 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const mongo = require('mongodb').MongoClient;
-//Creates new express
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+//Creates new constructors.
 var app = express();
+const SALT_WORK_FACTOR = 10;
 const port = 3000;
+var user_id = '';
 
 mongo.connect("mongodb://127.0.0.1/freecoders", function(err, db){
     if(err) throw err;
     let codedb = db.collection('freecoders');
+    var salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
 //View engine
 app.set('view engine', 'ejs');
 //Specify folder we want to use
@@ -20,36 +26,91 @@ app.set('views', path.join(__dirname, '../views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//Place to put static resources & paths
+
+//Express session middleware.
+app.use(cookieParser());
+app.use(session({
+    secret: 'cloud'
+}));
 
 
 //Handle a get request.
 app.get('/', function(req, res){
     //Renders the login page.
-    res.render('login', {
-        title: 'FreeCoders'
+    res.render('index', {
+        title: 'FreeCoders',
+        user: user_id
     });
-})
+});
+app.get('/login', function(req, res){
+    res.render('login', {
+        title: 'FreeCoders',
+        
+    });
+});
 //Catch Submition
 app.post('/users/add', function(req, res){
     console.log('Signup form submitted');
 
     //Converts all the recieved information into string and better variables.
+    
     var newUser = req.body.signupusr;
     var newEmail = req.body.signupemail;
     var newPwd = req.body.signuppwd;
 
+    //Hashing the password and place it into the database.
+    var hash = bcrypt.hashSync(newPwd, salt);
+    newPwd = hash;
+    console.log(newPwd);
     //Checks if the fields are empty or not.
     if(newUser != "" && newEmail != "" && newPwd != ""){
 
         //Inserts it into the correct database.
-    codedb.insert({user: newUser, email: newEmail});
+    codedb.insert({user: newUser, email: newEmail, password: newPwd});
     console.log('A user signed up with the username ' + newUser + ' with the email ' + newEmail);
+    console.log('Hashed the password: ' + newPwd);
     } else {
         console.log('Someone tried to signup with an empty field!'); //Logs that someone tried to signup with an empty field.
     }
     
 });
+app.post('/users/login', function(req, res){
+    console.log('Someoned tried to sign in.');
+
+    var usr = req.body.usr;
+    var pwd = req.body.pwd;
+
+    codedb.findOne({user: usr}, function(err, result){
+        if (err) throw err;
+        //If we get a result in the database then do this.
+        if(result != null){
+            //Hash the password to fit the database.
+            
+            
+             bcrypt.compare(pwd, result.password, function(err, response){
+                if(err) throw err;
+                if(response == true){
+                    //If we are logged in to the site.
+                    console.log('Someone successfully logged into the website. The username is: ' + usr);
+                    
+                    user_id = usr;
+                    req.session.userName = user_id;
+                    console.log(user_id);
+                    res.redirect('/');
+
+                } else {
+                    //OOPS, wrong password :///
+                    console.log(usr + ' tried to login with the wrong password.');
+                }
+             });
+             
+        } else {
+            console.log('Somone tried to login with a false username.');
+        }
+    });
+
+});
+
 
 //Listen for incooming connections on the port (3000)
 app.listen(port, function(){
